9ba8ba8f0c873d3f2e34ee565a45772bacfc3a06
DooYee <149545221+DOOYEE0709@users.noreply.github.com>
2025-04-07 08:52:00 +0900

    [Silver III] Title: 터렛, Time: 0 ms, Memory: 1128 KB -BaekjoonHub

--- C99/백준/Silver/1002. 터렛/터렛.c
+++ C99/백준/Silver/1002. 터렛/터렛.c
@@ -17,13 +17,13 @@ int main() {
         int sum_r = r1 + r2;
         int diff_r = abs(r1 - r2);
 
-        if (distance == 0 && r1 == r2) {
+        if (distance == 0 && r1 == r2) { // 두 원이 완전히 일치할 때, 교점 무수히 많음
             printf("-1\n");
-        } else if (distance > sum_r || distance < diff_r) {
+        } else if (distance > sum_r || distance < diff_r) { // 만나지 않음, 교점 0개
             printf("0\n");
-        } else if (distance == sum_r || distance == diff_r) {
+        } else if (distance == sum_r || distance == diff_r) { // 한 점에서 만남, (전자 외접, 후자 내접), 교점 1개
             printf("1\n");
-        } else {
+        } else { // 두 점에서 만남, 교점 2개
             printf("2\n");
         }
     }
