16a556cb69dc524d1f2b77579ba5b2fe3b2bf755
DooYee <149545221+DOOYEE0709@users.noreply.github.com>
2025-05-15 11:06:26 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/4153. 직각삼각형/직각삼각형.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    while (1)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+
+        if ((a == 0) && (b == 0) && (c == 0))
+        {
+            break;
+        }
+
+        int max = a;
+
+        if (b > max)
+        {
+            max = b;
+        }
+
+        if (c > max)
+        {
+            max = c;
+        }
+
+        int x, y;
+        if (max == a)
+        {
+            x = b;
+            y = c;
+        }
+        else if (max == b)
+        {
+            x = a;
+            y = c;
+        }
+        else
+        {
+            x = a;
+            y = b;
+        }
+
+        if ((max * max) == (x * x) + (y * y))
+        {
+            printf("right\n");
+        }
+        else
+        {
+            printf("wrong\n");
+        }
+    }
+
+    return 0;
+}
